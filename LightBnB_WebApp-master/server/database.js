d518df38b68c198333680818746e23885af13a78
kunvar13 <kunvar13@gmail.com>
2021-11-27 01:59:00 -0600

    Adding query to get all reservations

--- LightBnB_WebApp-master/server/database.js
+++ LightBnB_WebApp-master/server/database.js
@@ -65,7 +65,18 @@ exports.addUser = addUser;
  * @return {Promise<[{}]>} A promise to the reservations.
  */
 const getAllReservations = function(guest_id, limit = 10) {
-  return getAllProperties(null, 2);
+  return pool
+    .query(`SELECT reservations.id,  properties.title, properties.cost_per_night, reservations.start_date, AVG(property_reviews.property_id) AS average_rating
+            FROM reservations
+            JOIN users ON users.id = reservations.guest_id
+            JOIN properties ON properties.id = reservations.property_id
+            JOIN property_reviews ON property_reviews.property_id = reservations.property_id
+            WHERE users.id = $1 AND end_date < now()::date
+            GROUP BY reservations.id, properties.id LIMIT $2
+            ORDER BY reservations.start_date ASC`, [guest_id,limit])
+    .then((result) => result.rows)
+    .catch((err) =>{});
+
 };
 exports.getAllReservations = getAllReservations;
 
